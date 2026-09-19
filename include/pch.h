cdbfae02623a9891e41179799b4d7c75c84f6909
Ryo-Takahashi-0422 <rtakahashi0422@yahoo.co.jp>
2026-09-19 17:46:14 +0900

    フォルダ構造整理、pch作成

---
+++ include/pch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Windows.h>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <vulkan/vulkan.h>
+
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <memory>
+#include <functional>
--- src/GlfwDisplayWindow.cpp
+++ src/GlfwDisplayWindow.cpp
@@ -1,11 +1,10 @@
+#include "pch.h"
 #include <GlfwDisplayWindow.h>
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-#include <core/VulkanContext.h>
 #include <TriangleApp.h>
+#include <Windows.h>
 
 int __stdcall wWinMain(_In_ HINSTANCE hInstance,
-	_In_opt HINSTANCE hPrevInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR lpCmdLine,
 	_In_ int nCmdShow)
 {
